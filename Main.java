6bc97da7a11efa46bd2c982fab9b0185938e89e9
Dan Fabro <timpaxerror@gmail.com>
2021-12-03 06:21:49 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+// Java program to illustrate for loop
+public class Main {
+    public static void main(String args[])
+    {
+        int sum = 0;
+        for (int i = 1; i <= 10; i++){
+            sum += i;
+        }
+        System.out.println("Sum from 1 to 10 is " + sum);
+
+        sum = 0;
+        for (int i = 20; i <= 37; i++){
+            sum += i;
+        }
+        System.out.println("Sum from 20 to 37 is " + sum);
+
+
+        sum = 0;
+        for (int i = 35; i <= 49; i++){
+            sum += i;
+        }      
+        System.out.println("Sum from 35 to 49 is " + sum);   
+    }
+}
+
+accessModifier returnValueType methodName(list of parameters) {
+        // Method body;
+    }
+
